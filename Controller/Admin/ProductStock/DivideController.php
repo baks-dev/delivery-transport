8fd4448df3a7cb1d1eac4c086b4b64e57f5b2f3f
kingstar <admin@baks.dev>
2025-01-07 23:39:18 +0300

    Baks Development

--- Controller/Admin/ProductStock/DivideController.php
+++ Controller/Admin/ProductStock/DivideController.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -186,9 +186,6 @@ final class DivideController extends AbstractController
                 $interval = new DateInterval('P1D');
                 $date = $date->add($interval);
 
-                //                dump('Пробуем погрузку '.$deliveryDay);
-                //                dump($date);
-
                 if($deliveryDay > 30)
                 {
 
@@ -208,10 +205,6 @@ final class DivideController extends AbstractController
                  */
                 foreach($DeliveryTransportProfile as $keyTransport => $DeliveryTransportUid)
                 {
-                    //                    dump('Получаем путевой лист ');
-                    //                    dump($date);
-
-
                     $DeliveryPackageTransportDTO = new DeliveryPackageTransportDTO();
 
                     /**
