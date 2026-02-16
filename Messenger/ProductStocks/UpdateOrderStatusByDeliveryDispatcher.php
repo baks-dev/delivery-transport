11ec2d13540c5f7434b422de0fbd99edb7ac1e04
MichelAngelo <baksdevelopmen@gmail.com>
2026-02-16 14:56:57 +0300

    BaksDevelopment
    Signed-off-by: MichelAngelo <baksdevelopmen@gmail.com>

--- Messenger/ProductStocks/UpdateOrderStatusByDeliveryDispatcher.php
+++ Messenger/ProductStocks/UpdateOrderStatusByDeliveryDispatcher.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -139,8 +139,8 @@ final readonly class UpdateOrderStatusByDeliveryDispatcher
             ->send('orders');
 
         $this->logger->info(
-            'Обновили статус заказа на Delivery «Доставка»',
-            [self::class.':'.__LINE__, var_export($Order, true)],
+            sprintf('%s: Обновили статус заказа на Delivery «Доставка»', $ProductStockEvent->getNumber()),
+            [self::class.':'.__LINE__],
         );
 
     }
